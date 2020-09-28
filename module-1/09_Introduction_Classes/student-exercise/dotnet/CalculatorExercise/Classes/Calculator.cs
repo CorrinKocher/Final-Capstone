e616943c57cf2a790e3294a8806fa0b8d1f337ca
Corrin <corrin.kocher1@gmail.com>
2020-09-27 20:29:17 -0400

    thursday homework

--- module-1/09_Introduction_Classes/student-exercise/dotnet/CalculatorExercise/Classes/Calculator.cs
+++ module-1/09_Introduction_Classes/student-exercise/dotnet/CalculatorExercise/Classes/Calculator.cs
@@ -11,8 +11,46 @@ namespace TechElevator.Classes
     /// </summary>
     public class Calculator
     {
-       // public int MathExample (intx, int power)
-       // Math.Pow(x,power) = x raised to the power int)
-    }  // cannot convert double to an int. so cast the double to an int
-     // (int) Math.POW(x, power)
-}
+        // public int MathExample (intx, int power)
+        // Math.Pow(x,power) = x raised to the power int)
+        // (int) Math.POW(x, power)
+        // cannot convert double to an int. so cast the double to an int
+        private int result = 0;
+        private int addend = 0;
+        private int multiplier = 0;
+        private int exponent = 0;
+        public int Result { get; private set; }
+
+
+
+        public int Add(int addend)
+        {
+            Result = this.result + this.addend;
+            return result;
+        }
+        public int Multiply(int multiplier)
+        {
+            Result = result * this.multiplier;
+            return result;
+        }
+        public int Power(int exponent)
+
+        {
+            Result = result * result;
+            //double x;
+            // result = Math.Pow(x, (double)int exponent);
+            //return result;
+            return result;
+        }
+        public void Reset()
+        {
+            Result = 0;
+        }
+        public int Subtract(int subtrahend)
+        {
+            Result = this.result - subtrahend;
+            return result;
+        }
+
+    }
+} 
