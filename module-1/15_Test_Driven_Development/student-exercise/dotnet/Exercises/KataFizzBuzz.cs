a6d184df0e8156511ebb517ab853a09dff6c81c2
Corrin <corrin.kocher1@gmail.com>
2020-10-06 15:56:50 -0400

    adding KataFizzBuzz class

--- module-1/15_Test_Driven_Development/student-exercise/dotnet/Exercises/KataFizzBuzz.cs
+++ module-1/15_Test_Driven_Development/student-exercise/dotnet/Exercises/KataFizzBuzz.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class KataFizzBuzz
+    {
+        public string ReturnFizzBuzz(int number)
+        {
+
+
+            if (number <= 100 && number >= 1 && number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number <= 100 && number >= 1 && number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number <= 100 && number >= 1 && number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number >= 1 && number <= 100)
+            {
+                return number.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
