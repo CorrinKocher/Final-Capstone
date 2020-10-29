0cb6f319b0ac416ac64dd82b0a741ec9d10ba524
Corrin <corrin.kocher1@gmail.com>
2020-10-29 09:38:55 -0400

    morning pull

--- module-3/03-ServerSide_APIs_Part_1/student-exercise/dotnet/AuctionApp/Controllers/AuctionsController.cs
+++ module-3/03-ServerSide_APIs_Part_1/student-exercise/dotnet/AuctionApp/Controllers/AuctionsController.cs
@@ -16,10 +16,47 @@ namespace AuctionApp.Controllers
             this.dao = auctionDao;
         }
 
-        [HttpGet]
-        public List<Auction> GetAuctions()
+        //[HttpGet]
+        //public List<Auction> GetAuctions()
+        //{
+        //    return this.dao.List();
+        //}
+
+        [HttpGet("{id}")]
+        public Auction GetAuctionByID(int id)
         {
-            return this.dao.List();
+           
+            Auction auction = this.dao.Get(id);
+            if(auction != null)
+            {
+                return auction;
+            }
+            return null;
         }
+
+        [HttpGet]
+
+        public List<Auction> GetAuctions(string title_like = "", double currentBid_lte = 0.00)
+        {
+            if(!string.IsNullOrEmpty(title_like) && currentBid_lte != 0.00)
+            {
+                return this.dao.SearchByTitleAndPrice(title_like, currentBid_lte);
+            }
+
+            if (!string.IsNullOrEmpty(title_like))
+            {
+                return this.dao.SearchByTitle(title_like);
+            }
+           
+            if( currentBid_lte != 0.00)
+            {
+                return this.dao.SearchByPrice(currentBid_lte);
+            }
+            return this.dao.List();
+
+            
+        }
+
+        
     }
 }
