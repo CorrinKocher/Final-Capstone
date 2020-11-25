e878cd49a5f664e479849bdeeff04da82263c8ce
Corrin <corrin.kocher1@gmail.com>
2020-11-24 20:10:27 -0500

    friday hW

--- module-4/11_Vue_Event_Handling/student-exercise/src/components/UserList.vue
+++ module-4/11_Vue_Event_Handling/student-exercise/src/components/UserList.vue
@@ -15,7 +15,7 @@
       <tbody>
         <tr>
           <td>
-            <input type="checkbox" id="selectAll" />
+            <input type="checkbox" id="selectAll"  v-bind:checked="areAllSelected" v-on:change="selectAllUsers($event)"/>
           </td>
           <td>
             <input type="text" id="firstNameFilter" v-model="filter.firstName" />
@@ -44,7 +44,12 @@
           v-bind:class="{ disabled: user.status === 'Disabled' }"
         >
           <td>
-            <input type="checkbox" v-bind:id="user.id" v-bind:value="user.id" v-on:click="actionButtondisabled(user.id)" />
+            <input
+              type="checkbox"
+              v-bind:id="user.id"
+              v-bind:value="user.id"
+              v-model="selectedUserIDs"
+            />
           </td>
           <td>{{ user.firstName }}</td>
           <td>{{ user.lastName }}</td>
@@ -59,12 +64,12 @@
     </table>
 
     <div class="all-actions">
-      <button>Enable Users</button>
-      <button>Disable Users</button>
-      <button>Delete Users</button>
+      <button v-bind:disabled="actionButtonDisabled" v-on:click="enableSelectedUsers()">Enable Users</button>
+      <button v-bind:disabled="actionButtonDisabled" v-on:click="disableSelectedUsers()">Disable Users</button>
+      <button v-bind:disabled="actionButtonDisabled" v-on:click="deleteSelectedUsers()">Delete Users</button>
     </div>
 
-    <button v-on:click="showForm = true" >Add New User</button >
+    <button v-on:click="showForm = true">Add New User</button>
 
     <form id="frmAddNewUser" v-if="showForm">
       <div class="field">
@@ -73,17 +78,17 @@
       </div>
       <div class="field">
         <label for="lastName">Last Name:</label>
-        <input type="text" name="lastName" v-model.trim="newUser.lastName"/>
+        <input type="text" name="lastName" v-model.trim="newUser.lastName" />
       </div>
       <div class="field">
         <label for="username">Username:</label>
-        <input type="text" name="username" v-model.trim="newUser.username"/>
+        <input type="text" name="username" v-model.trim="newUser.username" />
       </div>
       <div class="field">
         <label for="emailAddress">Email Address:</label>
         <input type="text" name="emailAddress" v-model.trim="newUser.emailAddress" />
       </div>
-      <button type="submit" class="btn save" v-on:click="showForm = false, addUser()" >Save User</button>
+      <button type="submit" class="btn save" v-on:click="showForm = false, saveUser()">Save User</button>
     </form>
   </div>
 </template>
@@ -91,14 +96,11 @@
 <script>
 export default {
   name: "user-list",
- 
+
   data() {
     return {
-    
-    selectUserID: [],
-    showForm: false,
-    nextId: 6, 
-    thisUser: '',
+      selectedUserIDs: [],
+      showForm: false,
       filter: {
         firstName: "",
         lastName: "",
@@ -112,8 +114,7 @@ export default {
         lastName: "",
         username: "",
         emailAddress: "",
-        status: "Active",
-        
+        status: "Active"
       },
       users: [
         {
@@ -168,94 +169,120 @@ export default {
     };
   },
   methods: {
-    addUser () {
-       console.log('added new user');
+    enableSelectedUsers() {
+      this.users.forEach(user => {
+        this.selectedUserIDs.forEach(number => {
+          if (user.id === number) {
+            user.status = "Active";
+          }
+        }); 
+      }); 
+      this.selectedUserIDs = [];
+    },
+    disableSelectedUsers() {
+      this.users.forEach(user => {
+        this.selectedUserIDs.forEach(number => {
+          if (user.id === number) {
+            user.status = "Disabled";
+          }
+        }); 
+      });
+      this.selectedUserIDs = [];
+    },
+    deleteSelectedUsers(){
+      
+      this.selectedUserIDs.forEach((item) => {
+        
+        let user = this.users.findIndex((u) => {return item == u.id});
+       
+        this.users.splice(user, 1);
+      }); 
+      this.selectedUserIDs = [];
+    },
+    saveUser() {
+      console.log("added new user");
 
-      this.newUser.id = this.nextId;
-      this.nextId+= 1;
+      let nextId = this.users.length + 1;
+      this.newUser.id = nextId;
 
       this.users.unshift(this.newUser);
-      this.newUser = {
+      (this.newUser = {
         id: null,
-        firstName:'',
-        lastName: '',
-        username: '',
-        emailAddress: '',
-        status: '',
-      }
-      this.showForm = false;
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: ""
+      }),
+        (this.showForm = false);
     },
     flipStatus(id) {
-         
-      
-      //I get why thisUser has squigglies. because I am trying to give an object (one user in users) a name like 
-      //its a variable and its not. I dont know how to isolate THIS USER. it doesnt seem like I need the left
-      //side of the equation at all. but I also feel like I am not actually effectively doing anything with the right side
-      //Like sure, I am finding the individual user in the user array based off of the user's id. but I need to actually
-      //change the status. i dont see how find does what I want. Can I do this>>> if(this.users.find(u => u.id === id))
-      //then give it a body/scope and say this.user.status === whatever they heck i want etc. nvmd tried it, didnt work
-      //also i know its mad that I am not doing anything with thisUser. but I tried that too. called thisUser.status= and that 
-      //didnt work
-      //Leslie then said I have to include whatever I have in script in 2 places?? is that right? I added it on line 99
-      //but I cant understand why i wont need to do that. i also confirmed on the console vue thing that the objects status 
-      // is not changing
+      let thisUser = this.users.find(u => u.id === id);
 
-    let thisUser = this.users.find(u => u.id === id); 
-      
-          if(thisUser.status === 'Active') {
-        thisUser.status = 'Disabled';
+      if (thisUser.status === "Active") {
+        thisUser.status = "Disabled";
+      } else {
+        thisUser.status = "Active";
+      }
+    },
+    selectAllUsers(e) {
+      if(e.target.checked) {
+        this.selectedUserIDs = this.users.map;
       }
       else {
-        thisUser.status = 'Active';
-      }      
-     
-      
-    }    
+        this.selectedUserIDs = [];
+      }
+    }
+
   },
   computed: {
-    actionButtonDisabled(selectUserID, id) {
-      let thisUser = this.users.find(u => u.id === id); 
-      if( this.selectUserID.includes(thisUser)){
-        selectUserID.push(thisUser);
+    areAllSelected() {
+      if(this.selectedUserIDs.length == this.users.length) {
+        return true;
       }
-      else{
-        selectUserID.pop(thisUser);
+      else {
+        return false;
       }
-      return this.selectUserID;
+    },
+    actionButtonDisabled() {
+      if (this.selectedUserIDs.length) {
+        return false;
+      }
+      return true;
     },
     filteredList() {
       let filteredUsers = this.users;
       if (this.filter.firstName != "") {
-        filteredUsers = filteredUsers.filter((user) =>
+        filteredUsers = filteredUsers.filter(user =>
           user.firstName
             .toLowerCase()
             .includes(this.filter.firstName.toLowerCase())
         );
       }
       if (this.filter.lastName != "") {
-        filteredUsers = filteredUsers.filter((user) =>
+        filteredUsers = filteredUsers.filter(user =>
           user.lastName
             .toLowerCase()
             .includes(this.filter.lastName.toLowerCase())
         );
       }
       if (this.filter.username != "") {
-        filteredUsers = filteredUsers.filter((user) =>
+        filteredUsers = filteredUsers.filter(user =>
           user.username
             .toLowerCase()
             .includes(this.filter.username.toLowerCase())
         );
       }
       if (this.filter.emailAddress != "") {
-        filteredUsers = filteredUsers.filter((user) =>
+        filteredUsers = filteredUsers.filter(user =>
           user.emailAddress
             .toLowerCase()
             .includes(this.filter.emailAddress.toLowerCase())
         );
       }
       if (this.filter.status != "") {
-        filteredUsers = filteredUsers.filter((user) =>
-          user.status === this.filter.status
+        filteredUsers = filteredUsers.filter(
+          user => user.status === this.filter.status
         );
       }
       return filteredUsers;
